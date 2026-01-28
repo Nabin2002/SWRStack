df33bcb50155a6dfde33a06e9c2ec0dd44c8f267
Nabin Chandra Roy <nabinchandrar3@gmail.com>
2026-01-29 00:32:46 +0530

    Prepare frontend for deployment

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, type ChangeEvent, type FormEvent } from 'react'
 import 'remixicon/fonts/remixicon.css'
 import axios from 'axios'
 import useSWR, { mutate } from 'swr'
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 const fetcher = async (url: string) => {
   try {
     const { data } = await axios.get(url)
