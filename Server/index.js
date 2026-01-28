1600eb9a4d1a9e769085b025326557ced33525ea
Nabin Chandra Roy <nabinchandrar3@gmail.com>
2026-01-28 23:39:19 +0530

    Change in CORS policy

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
